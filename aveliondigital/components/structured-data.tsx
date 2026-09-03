583338aacfcf251a5c9d5041b597f71766797cee
danielecitran <daniele.ctr.zh@icloud.com>
2026-09-03 18:58:50 +0200

    build fix

--- aveliondigital/components/structured-data.tsx
+++ aveliondigital/components/structured-data.tsx
@@ -1,22 +1,28 @@
 type JsonLdObject = Record<string, unknown>;
 type JsonLd = JsonLdObject | ReadonlyArray<JsonLdObject>;
 
+function isJsonLdArray(data: JsonLd): data is ReadonlyArray<JsonLdObject> {
+  return Array.isArray(data);
+}
+
 function stripContext(item: JsonLdObject): JsonLdObject {
   const { ["@context"]: _context, ...rest } = item;
   return rest;
 }
 
 function toDocument(data: JsonLd): JsonLdObject {
-  if (!Array.isArray(data)) {
-    return data["@context"]
-      ? data
-      : { "@context": "https://schema.org", ...data };
+  if (isJsonLdArray(data)) {
+    return {
+      "@context": "https://schema.org",
+      "@graph": data.map(stripContext),
+    };
   }
 
-  return {
-    "@context": "https://schema.org",
-    "@graph": data.map(stripContext),
-  };
+  if ("@context" in data) {
+    return data;
+  }
+
+  return { "@context": "https://schema.org", ...data };
 }
 
 export function StructuredData({ data }: { data: JsonLd }) {
